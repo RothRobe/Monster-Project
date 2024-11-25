d7f9b626fdbebb4b018293480384e25db84ad4ff
Robert <s2roroth@uni-trier.de>
2024-11-25 15:55:43 +0900

    Improved physics

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    private readonly Vector2 _velocityVector = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class BallScript : MonoBehaviour
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (collision.gameObject.CompareTag("Paddle"))
         {
+            rb.velocity *= _velocityVector;
             // Add upward force when hitting the paddle
             Vector3 bounce = new Vector3(0, bounceForce, 0);
             rb.AddForce(bounce, ForceMode2D.Impulse);
